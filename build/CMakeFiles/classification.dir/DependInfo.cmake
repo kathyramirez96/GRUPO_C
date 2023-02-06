421c7f46a007ae0371e15e2a7be3c78e3c8a7391
kathyramirez96 <kathyramirez_96@hotmail.com>
2023-02-06 17:19:55 -0500

    final

---
+++ build/CMakeFiles/classification.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/PARALELA/PADRE/classification/GLCM/AVEREGE.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/AVEREGE.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/CONTRASTE.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/CONTRASTE.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/CORRELATIONCOEFICIENT.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/CORRELATIONCOEFICIENT.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/ENTROPIA.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/ENTROPIA.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/MESURE.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/MESURE.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/MOEMNT.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/MOEMNT.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/SECONDANGULAR.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/SECONDANGULAR.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/SQUAREVARIANCE.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/SQUAREVARIANCE.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/VARIANCE.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/VARIANCE.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/benchmark.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/benchmark.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM/imagenes.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM/imagenes.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/AVEREGE_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/AVEREGE_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/CONTRASTE_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/CONTRASTE_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/CORRELATIONCOEFICIENT_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/CORRELATIONCOEFICIENT_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/ENTROPIA_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/ENTROPIA_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/MESURE_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/MESURE_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/MOEMNT_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/MOEMNT_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/SECONDANGULAR_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/SECONDANGULAR_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/SQUAREVARIANCE_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/SQUAREVARIANCE_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/VARIANCE_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/VARIANCE_P.o"
+  "/home/user/PARALELA/PADRE/classification/GLCM_P/imagenes_P.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/GLCM_P/imagenes_P.o"
+  "/home/user/PARALELA/PADRE/classification/dicom_read/DicomReader.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/dicom_read/DicomReader.o"
+  "/home/user/PARALELA/PADRE/classification/dicom_read/dicomutils.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/dicom_read/dicomutils.o"
+  "/home/user/PARALELA/PADRE/classification/fcm/fcm.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/fcm/fcm.o"
+  "/home/user/PARALELA/PADRE/classification/knn/Preprocessing.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/Preprocessing.o"
+  "/home/user/PARALELA/PADRE/classification/knn/ReadDataset.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/ReadDataset.o"
+  "/home/user/PARALELA/PADRE/classification/knn/dataset.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/dataset.o"
+  "/home/user/PARALELA/PADRE/classification/knn/knn.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/knn.o"
+  "/home/user/PARALELA/PADRE/classification/knn/knnresults.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/knnresults.o"
+  "/home/user/PARALELA/PADRE/classification/knn/matrix.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/knn/matrix.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/ArgumentHandler.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/ArgumentHandler.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/Data.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/Data.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/Forest.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/Forest.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/ForestClassification.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/ForestClassification.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/ForestProbability.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/ForestProbability.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/ForestRegression.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/ForestRegression.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/ForestSurvival.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/ForestSurvival.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/Tree.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/Tree.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/TreeClassification.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/TreeClassification.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/TreeProbability.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/TreeProbability.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/TreeRegression.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/TreeRegression.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/TreeSurvival.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/TreeSurvival.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/rf.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/rf.o"
+  "/home/user/PARALELA/PADRE/classification/random_forest/utility.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/random_forest/utility.o"
+  "/home/user/PARALELA/PADRE/classification/svm/svm.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/svm/svm.o"
+  "/home/user/PARALELA/PADRE/classification/svm/svm_util.cpp" "/home/user/PARALELA/PADRE/build/CMakeFiles/classification.dir/classification/svm/svm_util.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../classification"
+  "../metrics"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
